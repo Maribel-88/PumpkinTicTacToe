af8f99dff703bd37f2e0a2082d80afa7cb6f3a3e
Maribel1988 <102556943+Maribel1988@users.noreply.github.com>
2024-03-25 16:37:06 +0000

    resize the images

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -24,7 +24,7 @@ function placeXOrO(squareNumber) {
         // Active player may only be 'X' or 'O' so, if not 'X' it must be 'O'    
         } else {
         //if activePlayer is equal to 'O', the o.png is placed in HTML     
-            select.style.backgroundImage = 'url("images/o2.png")';
+            select.style.backgroundImage = ' url("images/o2.png")';
         }
         //squareNumber and activePlayer are concatenated together together and added to array.
         selectedSquares.push(squareNumber + activePlayer);
