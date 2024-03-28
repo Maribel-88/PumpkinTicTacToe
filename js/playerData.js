380e1ec01641f413e43bfa77658fae1e5b1f97e3
Maribel1988 <102556943+Maribel1988@users.noreply.github.com>
2024-03-28 08:10:59 +0000

    combined all js code in one file, still have the issue regarding the image, can't figureout how to set the image to the x or o player.

--- js/playerData.js
+++ js/playerData.js
@@ -1,4 +1,4 @@
-
+/*
 let userInfo = document.getElementById("playerInfo");
 let firstPlayer = document.getElementById("playerName");
 let secondPlayer = document.getElementById("player2Name");
@@ -48,17 +48,11 @@ let generatePlayerData = () => {
                             <img width="50" height="50" src=${img} alt="">
                             <div class="details">
                             <h3>${name}</h3>
-                            <button id=${id} type="button" class="btn btn-light quantity" onclick="choosePlayer(${id})">Choose</button>
+                            <button id=${id} type="button" class="btn btn-light choose" onclick="choosePlayer(${id})">Choose</button>
                             </div>
                      </div>
-
-                     
-   
   `;
-    })
-
-        .join(""));
-  
+    }) .join("")); 
 };
 generatePlayerData(); 
 
@@ -70,43 +64,29 @@ let choosePlayer = (id) => {
     let playerImage= playerData.find((x)=> x.id === selectedPlayer.id);
     playerFolder.push(playerImage);
     let search = playerData.find((x)=> x.id !== selectedPlayer.id);
-    secondPlayer.innerHTML = `<div id=product-id-${id} class="item">
-    <h4>Random</h4>
-    <img id="player2Image" width="50" height="50" src=${search.img} alt="">
-    <div class="details">
-      <h3 id="playerName2">${search.name}</h3>
-     
-      <div class="price-quantity">
-       
-        <div class="btn-group btn-group-sm " role="group" aria-label="Basic example">
-            <button onclick="decrement(${id})" type="button" class="btn btn-light bi bi-dash-lg"></button>
-            <button id=${id} type="button" class="btn btn-light quantity"></button>
-            <button onclick="increment(${id})" type="button" class="btn btn-light bi bi-plus-lg"></button>
+    secondPlayer.innerHTML = 
+    `<div id=product-id-${id} class="item">
+        <h4>Random</h4>
+        <img id="player2Image" width="50" height="50" src=${search.img} alt="">
+        <div class="details">
+          <h3 id="playerName2">${search.name}</h3>
+          <div id="score2">Score:</div>
         </div>
-      </div>
-    </div>
-  </div>
+     </div>
   ` ; 
     return firstPlayer.innerHTML =
     `<div id=product-id-${id} class="item">
-     
-     <img id="player1Image" width="50" height="50" src=${playerImage.img} alt="">
-        <h5 id="playerName">${firstPlayerData[0].playerName1} "the ${playerImage.name}"</h5>
-       
-       
-          <div class="price-quantity">
-           
-            <div class="btn-group btn-group-sm " role="group" aria-label="Basic example">
-                <button onclick="decrement(${id})" type="button" class="btn btn-light bi bi-dash-lg"></button>
-                <button id=${id} type="button" class="btn btn-light quantity"></button>
-                <button onclick="increment(${id})" type="button" class="btn btn-light bi bi-plus-lg"></button>
-            </div>
-          </div>
+        <h4>${firstPlayerData[0].playerName1}</h4>
+        <img id="player1Image" width="50" height="50" src=${playerImage.img} alt="">
+        <div class="details">
+          <h3 id="playerName">${playerImage.name}</h3>
+          <div id="score1">Score:</div>
         </div>
-      </div>
-      `;
+     </div>
+  `
+        ;
     
     }
     choosePlayer();
-    
-   
+    gameCanvas();
+   */
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -9,8 +9,91 @@ let restartBtn = document.getElementById("restart");
 let msgRef = document.getElementById("message");
 let newgameBtn = document.getElementById("new-game"); 
 let popupRef = document.querySelector(".popup");
-let skullImage = document.getElementById('player1Image').value;
-let skull = playerFolder.find((x)=>x.img);
+let userInfo = document.getElementById("playerInfo");
+let firstPlayer = document.getElementById("playerName");
+let secondPlayer = document.getElementById("player2Name");
+let userImage = document.getElementById('player1Image').value;
+
+
+let playerFolder = []; //chosen character data is stored
+let firstPlayerData = []; // user name input is stored
+let playerData = [
+    {
+        id:"fhgfdhrrvr",
+        name:"Skull Pirate",
+        img:"images/x2.png"
+    },
+  
+    {
+        id:"fghtjtjtkh",
+        name:"Pumpkin Head",
+        img:"images/o2.png"
+    }
+  ];  
+  
+
+//push the user input , user name to firstPlayerData
+const addUser = (event)=>{
+    let user = {playerName1:document.getElementById("firstPlayerName").value}
+    firstPlayerData.push(user);
+    document.forms[0].reset(); // to clear the form for the next entries      
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('userSaveBtn').addEventListener('click', addUser);
+});
+
+const generatePlayerData = () => {
+    return(userInfo.innerHTML = playerData.map((x)=>{
+            let {id, name, img } = x
+            return ` 
+                     <div id=player-id-${id} class="item">
+                            <img width="50" height="50" src=${img} alt="">
+                            <div class="details">
+                            <h3>${name}</h3>
+                            <button id=${id} type="button" class="btn btn-light choose" onclick="choosePlayer(${id})">Choose</button>
+                            </div>
+                     </div>
+  `;
+    }) .join("")); 
+};
+generatePlayerData(); 
+
+
+let choosePlayer = (id) => {
+    
+    let selectedPlayer= id;
+    
+    let playerImage= playerData.find((x)=> x.id === selectedPlayer.id);
+    playerFolder.push(playerImage);
+    let search = playerData.find((x)=> x.id !== selectedPlayer.id);
+    secondPlayer.innerHTML = 
+    `<div id=product-id-${id} class="item">
+        <h4>Random</h4>
+        <img width="50" height="50" src=${search.img} alt="">
+        <div class="details">
+          <h3 id="playerName2">${search.name}</h3>
+          <div id="score2">Score:</div>
+        </div>
+     </div>
+  ` ; 
+    return firstPlayer.innerHTML =
+    `<div id=product-id-${id} class="item">
+        <h4>${firstPlayerData[0].playerName1}</h4>
+        <img  width="50" height="50" src=${playerImage.img} alt="">
+        <div class="details">
+          <h3 id="playerName">${playerImage.name}</h3>
+          <div id="score1">Score:</div>
+        </div>
+     </div>
+  `
+        ;
+        
+    }
+    
+    choosePlayer();
+
+console.log(userImage);
 //This function is for placing an x or o in a square.
 function placeXOrO(squareNumber) {
     //this condition ensures a square hasn't been selected already.
@@ -21,8 +104,9 @@ function placeXOrO(squareNumber) {
         let select = document.getElementById(squareNumber);
         //This condition checks who's turn it is
         if (activePlayer==='X') {
+            
         //If activePlayer is equal to 'X', the x.png is placed in HTML.
-            select.style.backgroundImage = 'url("images/x2.png")';
+            select.style.backgroundImage = ' url("images/x2.png")';
         // Active player may only be 'X' or 'O' so, if not 'X' it must be 'O'    
         } else {
         //if activePlayer is equal to 'O', the o.png is placed in HTML     
@@ -250,7 +334,7 @@ animateLineDrawing();
 setTimeout(function() {clear(); resetGame(); }, 1000);
 }
 //This function resets the game in a tie or a win.
-function resetGame() {
+const resetGame = () => {
     for (let i = 0; i < 9; i++) {
         //This for loop iterates through each HTML square element
         let square = document.getElementById(String(i));
